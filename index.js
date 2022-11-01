7906aea909860fd6ec3f27fb1fa86df5fa6f2e73
Allie Soldau <alliesoldau@gmail.com>
2022-11-01 12:52:24 -0400

    commit tree 4710f9579e87df4eb003754fc83ff4dc1c3e5b9d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const newArray = []
+    for(let i = 0; i < array.length; i++) {
+        console.log(i);
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        console.log(array[i]);
+    }
+    return newArray;
+}
+
+function countDown(integer) {
+    let counter = 0;
+    while (counter <= integer) {
+        console.log(integer - counter);
+        counter++;
+    }
+}
